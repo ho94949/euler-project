01cebe4110478699d3896b3a9c196e8db0c23598
ho94949 <sakura@kyouko.moe>
2017-04-24 03:09:28 +0900

    Library Updated
    Added Tau function (sigma_0)

--- lib.py
+++ lib.py
@@ -13,3 +13,24 @@ def isPrime(x):
         if x%i==0: return False
         i += 2
     return True
+
+"""
+function         tau
+Version          1
+Time Complexity  O(sqrt(x))
+Description
+factorize and product of (ki+i)s 
+"""
+def tau(x):
+    if x<1: return 0 
+    i = 2 
+    ans = 1
+    while i*i <= x:
+        k = 1
+        while x%i == 0:
+            x //= i 
+            k += 1 
+        ans *= k 
+        i += 1
+    if x != 1: ans *= 2
+    return ans
